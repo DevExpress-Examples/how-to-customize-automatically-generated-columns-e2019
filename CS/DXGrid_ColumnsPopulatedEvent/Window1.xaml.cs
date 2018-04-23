4140299c10d6deb57aaa5b8d3c59aa6f434ebd0a
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/DXGrid_ColumnsPopulatedEvent/Window1.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Documents;
+using DevExpress.Wpf.Grid;
+
+namespace DXGrid_ColumnsPopulatedEvent {
+    public partial class Window1 : Window {
+        public Window1() {
+            InitializeComponent();
+            grid.DataSource = IssueList.GetData();
+        }
+        private void grid_ColumnsPopulated(object sender, RoutedEventArgs e) {
+            foreach (GridColumn column in grid.Columns)
+                if (column.FieldName == "ID")
+                    column.Visible = false;
+            grid.View.VisibleColumns[0].SortOrder = DevExpress.Data.ColumnSortOrder.Descending;
+        }
+        public class IssueList {
+            static public List<IssueDataObject> GetData() {
+                List<IssueDataObject> data = new List<IssueDataObject>();
+                data.Add(new IssueDataObject() { ID = 0, 
+                    IssueName = "Transaction History", IssueType = "Bug", IsPrivate = true });
+                data.Add(new IssueDataObject() { ID = 1,
+                    IssueName = "Ledger: Inconsistency", IssueType = "Bug", IsPrivate = false });
+                data.Add(new IssueDataObject() { ID = 2,
+                    IssueName = "Data Import", IssueType = "Request", IsPrivate = false });
+                data.Add(new IssueDataObject() { ID = 3,
+                    IssueName = "Data Archiving", IssueType = "Request", IsPrivate = true });
+                return data;
+            }
+        }
+        public class IssueDataObject {
+            public int ID { get; set; }
+            public string IssueName { get; set; }
+            public string IssueType { get; set; }
+            public bool IsPrivate { get; set; }
+        }
+    }
+}
